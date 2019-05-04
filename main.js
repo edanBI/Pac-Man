0bf34f11f39c01e572d488fb7c32a5ec19d0f780
edanBI <idan.ivri@gmail.com>
2019-05-04 18:14:08 +0300

    game: right buttons fixed

--- main.js
+++ main.js
@@ -1026,3 +1026,10 @@ $(document).ready(function () {
 		}
 	});
 });
+
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
